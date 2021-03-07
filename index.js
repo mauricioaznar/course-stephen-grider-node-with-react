e3b12e5995e42df2cac1a188e9ecd92594f75bcc
Maguas Tupaguas <mauricioaznar94@gmail.com>
2021-03-07 14:20:56 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({
+    hi: 'there'
+  })
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`)
+})
