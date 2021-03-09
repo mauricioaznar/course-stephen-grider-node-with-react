c1a68322c05f5d7e2fd25ca7cb7428afab694ef9
Maguas Tupaguas <mauricioaznar94@gmail.com>
2021-03-09 15:33:47 -0600

    prod build

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,8 @@
 module.exports = {
   googleClientID: '110865774611-9amnmdug156iesivb2hc3gtdl69dqk0u.apps.googleusercontent.com',
   googleClientSecret: 'PYRfJucfZ36ieoOnkn1yg1U4',
-  mongoURI: 'mongodb+srv://maguas:maguas@cluster0.3b7p1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  cookieKey: 'asdfasdfasdfasdfahsdfhllkjdflkahslkdfhlakshdflkhalkh'
+  mongoURI: 'mongodb+srv://maguas:maguas@emaily-cluster.dro2u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  cookieKey: 'asdfasdfasdfasdfahsdfhllkjdflkahslkdfhlakshdflkhalkh',
+  stripePublishableKey: 'pk_test_51IOCYBI3rZ17NZyMpTArlvyf2K0bloNrBdCSRIvCvVZuOMDwkmMUkytso4vWFI1GcXTGV2INNpsjpPX9oSDnFoNV00vdibcddH',
+  stripeSecretKey: 'sk_test_51IOCYBI3rZ17NZyMDwhlB5lJlcML0vFbmtsRJIYLicDdt7D2uLGxePNmNUWfxztm1BVdyz1abzPtegjYCbVLYOTa005rBjwmw2'
 }
